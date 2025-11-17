98c695c0a8af4114ea1d008630cda1e6c26e2b04
Cristiano Silva Souza <msp.cris@gmail.com>
2025-11-17 15:43:00 -0300

    Ajuste no select para ver dados dos médicos em C

--- SQL/fin_despesa_medicos.sql
+++ SQL/fin_despesa_medicos.sql
@@ -1,11 +1,11 @@
 SELECT d.iddespesa, d.valor AS medicos
 FROM Fin_Despesa d
 JOIN fin_plano     fp ON fp.idplano = d.idPlano
-                     AND fp.Descricao = 'MEDICINA CONVENCIONAL'
 JOIN fin_contatipo ct ON ct.idcontatipo = d.idContaTipo
-                     AND ct.Tipo = 'médico - hora médica'
 WHERE d.idContaTipo <> 11
-  AND d.DataPagamentoAuto >= :ini
-  AND d.DataPagamentoAuto <  :fim
+  AND d.DataPagamentoAuto >= '01/10/2025'
+  AND d.DataPagamentoAuto <  '31/10/2025'
   AND d.DataPagamento IS NOT NULL
   AND (d.DataCancelamento IS NULL OR d.DataCancelamento = '')
+  AND (fp.Descricao = 'MEDICINA CONVENCIONAL' or  fp.Descricao like 'sal%')
+  AND (ct.Tipo = 'plantão medico' or ct.Tipo = 'médico - hora médica')
