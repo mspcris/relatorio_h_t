c9ee538f320c88f6b2a3d3ef02be2fcc9bc13873
Cristiano Silva Souza <msp.cris@gmail.com>
2025-11-17 16:43:56 -0300

    ajuste no select de alimentacao para contemplar os postos CDMJP

--- SQL/fin_despesa_alimentacao.sql
+++ SQL/fin_despesa_alimentacao.sql
@@ -1,6 +1,11 @@
 Select d.[iddespesa], d.[valor] as alimentacao from Fin_Despesa d
-Where idContaTipo<>11
+left join Fin_Plano p on p.idPlano = d.idPlano
+left join Fin_ContaTipo ct on ct.idContaTipo = d.idContaTipo
+Where d.idContaTipo<>11
  and d.[DatapagamentoAuto] >= :ini
  and d.[DatapagamentoAuto] < :fim
  and (D .DataPagamento IS NOT NULL and D .DataCancelamento IS NULL)
- and d.[idPlano] = (select idplano from fin_plano fp where fp.Descricao = 'alimentação funcionários' and desativado = 0)
+ and p.Descricao in ('alimentação funcionários', 'DESPESAS FIXAS', 'DESPESAS DIVERSAS', 'COMPRAS DIVERSAS')
+ and ct.Tipo in ('ALIMENTAÇÃO CAFÉ/LANCHE', 'ALIMENTAÇÃO ALMOÇO/JANTA', 'PADARIA', 'REFEICAO')
+
+ --(select idplano from fin_plano fp where fp.Descricao = 'alimentação funcionários' and desativado = 0)
