62b2fe114dcfa782219c7aa37686ebd43dfb72b4
Cristiano Silva Souza <msp.cris@gmail.com>
2025-12-01 16:56:24 -0300

    Forçando update da VM

--- sql/fin_receita_mensalidades.sql
+++
@@ -1,6 +0,0 @@
-Select r.[idreceita], r.[ValorPago] as Mensalidades from fin_receita r
-Where r.[DataPagamentoAuto] >= :ini
-  and r.[DataPagamentoAuto] < :fim    
-  and r.[DataPagamento] IS NOT NULL
-  and r.[idcontaTipo] = 5
-  and dataabono is null
